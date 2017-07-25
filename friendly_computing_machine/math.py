19d220596b17d204b2a9512a8d8fd5a9e033e23b
Courtney Fitzgerald <cefitzge@buffalo.ed>
2017-07-25 15:01:19 -0400

    test works

---
+++ friendly_computing_machine/math.py
@@ -0,0 +1,18 @@
+"""
+A small set of functions for doing math operations.
+"""
+
+#Write a function named add that adds two values
+def add(A,B):
+    """
+    Function that adds two arguements
+    """
+    return 2 + 5
+
+#Write  function that multiplies two values 
+def mult(A,B):
+    """
+    Function that multiplies two arguements
+    """
+    return 2 * 5
+
