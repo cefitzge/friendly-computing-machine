8b61f68feff781dc03d1b59db62ccfeffbd81fdb
Courtney Fitzgerald <cefitzge@buffalo.ed>
2017-07-25 15:38:06 -0400

    new testdata

--- tests/test_math.py
+++ tests/test_math.py
@@ -7,3 +7,11 @@ import pytest
 def test_add():
     assert fcm.math.add(5,2) == 7
     assert fcm.math.add(2,5) == 7
+   # assert fcm.math.add(1,2) == 3
+
+testdata = [
+    (2, 5, 10)
+    (1, 1, 2)
+    (0, 0, 0)
+]
+#@pytest.mark.parametrize("a, b, expected")
